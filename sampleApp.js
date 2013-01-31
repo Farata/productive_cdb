2645ae703317719d1748c05598322eb1ae9c0abb
Viktor Gamov <gamussa@gmail.com>
2013-01-31 01:41:56 -0500

    merged external review. Added intro to part 3

---
+++ sampleApp.js
@@ -0,0 +1,88 @@
+Ext.Loader.setConfig({
+	disableCaching : false,
+	enabled : true,
+	paths : {
+		episode_3_pagination : 'app',
+		Clear : 'clear'
+	}
+});
+
+Ext.syncRequire('episode_3_pagination.init.InitDirect');
+// Define GridPanel
+var myStore = Ext.create('episode_3_pagination.store.dto.PersonStore',{});
+Ext.define('episode_3_pagination.view.SampleGridPanel', {
+	extend : 'Ext.grid.Panel',
+	store : myStore,
+	alias : 'widget.samplegridpanel',
+	autoscroll : true,
+	plugins : [{
+		ptype : 'cellediting'
+	}],
+	dockedItems: [
+	{
+		xtype: 'pagingtoolbar',
+		displayInfo: true,
+		dock: 'top',
+		store: myStore
+	}
+	],
+	columns : [{
+		header : 'firstName',
+		dataIndex : 'firstName',
+		editor : {
+			xtype : 'textfield'
+		},
+		flex : 1
+	}, {
+		header : 'id',
+		dataIndex : 'id',
+		flex : 1
+	}, {
+		header : 'lastName',
+		dataIndex : 'lastName',
+		editor : {
+			xtype : 'textfield'
+		},
+		flex : 1
+	}, {
+		header : 'phone',
+		dataIndex : 'phone',
+		editor : {
+			xtype : 'textfield'
+		},
+		flex : 1
+	}, {
+		header : 'ssn',
+		dataIndex : 'ssn',
+		editor : {
+			xtype : 'textfield'
+		},
+		flex : 1
+	}],
+	tbar : [{
+		text : 'Load',
+		action : 'load'
+	}, {
+		text : 'Add',
+		action : 'add'
+	}, {
+		text : 'Remove',
+		action : 'remove'
+	}, {
+		text : 'Sync',
+		action : 'sync'
+	}]
+});
+// Launch the application
+Ext.application({
+	name : 'episode_3_pagination',
+	requires : ['Clear.override.ExtJSOverrider'],
+	controllers : ['SampleController'],
+	launch : function() {
+		Ext.create('Ext.container.Viewport', {
+			items : [{
+				xtype : 'samplegridpanel'
+			}]
+		});
+	}
+});
