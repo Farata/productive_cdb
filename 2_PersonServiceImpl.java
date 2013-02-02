0c11646580783f334de4f36e97f40eb2f278ca5d
Viktor Gamov <gamussa@gmail.com>
2013-02-01 23:56:22 -0500

    fixed images numeration

--- 2_PersonServiceImpl.java
+++ 2_PersonServiceImpl.java
@@ -6,34 +6,34 @@ import clear.data.ChangeObject;
 import dto.Person;
 import service.generated.*;
 
-public class PersonServiceImpl extends _PersonServiceImpl {
+public class PersonServiceImpl extends _PersonServiceImpl { // <1>
 
 	@Override
-	public List<Person> getPersons() {
-		List<Person> result = new ArrayList<>();
+	public List<Person> getPersons() {				// <2>
+		List<Person> result = new ArrayList<>();	
 		Integer id= 0;
 		result.add(new Person(++id, "Joe", "Doe", "555-55-55", "1111-11-1111"));
 		result.add(new Person(++id, "Joe", "Doe", "555-55-55", "1111-11-1111"));
 		result.add(new Person(++id, "Joe", "Doe", "555-55-55", "1111-11-1111"));
 		result.add(new Person(++id, "Joe", "Doe", "555-55-55", "1111-11-1111"));
-		return result;
+		return result;		//<3>
 	}
 
 	@Override
-	public void getPersons_doCreate(ChangeObject changeObject) {
+	public void getPersons_doCreate(ChangeObject changeObject) {  // <4>
 		// TODO Auto-generated method stub
-		changeObject.getNewVersion();
+		changeObject.getNewVersion();				
 		super.getPersons_doCreate(changeObject);
 	}
 
 	@Override
-	public void getPersons_doUpdate(ChangeObject changeObject) {
+	public void getPersons_doUpdate(ChangeObject changeObject) {	// <5>
 		// TODO Auto-generated method stub
 		super.getPersons_doUpdate(changeObject);
 	}
 
 	@Override
-	public void getPersons_doDelete(ChangeObject changeObject) {
+	public void getPersons_doDelete(ChangeObject changeObject) {	// <6>
 		// TODO Auto-generated method stub
 		super.getPersons_doDelete(changeObject);
 	}
