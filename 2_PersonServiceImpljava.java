29435a05c848c8ffbeccd4c848b5a3cc1de32706
Viktor Gamov <gamussa@gmail.com>
2013-02-01 22:56:43 -0500

    renamed sources from part1 and part2

---
+++ 2_PersonServiceImpljava.java
@@ -0,0 +1,41 @@
+package service;
+import java.util.ArrayList;
+import java.util.List;
+
+import clear.data.ChangeObject;
+import dto.Person;
+import service.generated.*;
+
+public class PersonServiceImpl extends _PersonServiceImpl {
+
+	@Override
+	public List<Person> getPersons() {
+		List<Person> result = new ArrayList<>();
+		Integer id= 0;
+		result.add(new Person(++id, "Joe", "Doe", "555-55-55", "1111-11-1111"));
+		result.add(new Person(++id, "Joe", "Doe", "555-55-55", "1111-11-1111"));
+		result.add(new Person(++id, "Joe", "Doe", "555-55-55", "1111-11-1111"));
+		result.add(new Person(++id, "Joe", "Doe", "555-55-55", "1111-11-1111"));
+		return result;
+	}
+
+	@Override
+	public void getPersons_doCreate(ChangeObject changeObject) {
+		// TODO Auto-generated method stub
+		changeObject.getNewVersion();
+		super.getPersons_doCreate(changeObject);
+	}
+
+	@Override
+	public void getPersons_doUpdate(ChangeObject changeObject) {
+		// TODO Auto-generated method stub
+		super.getPersons_doUpdate(changeObject);
+	}
+
+	@Override
+	public void getPersons_doDelete(ChangeObject changeObject) {
+		// TODO Auto-generated method stub
+		super.getPersons_doDelete(changeObject);
+	}
+	
+}
