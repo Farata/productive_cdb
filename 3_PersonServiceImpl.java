e02235535f04d1146327a2a90d5bc023278c7228
Viktor Gamov <gamussa@gmail.com>
2013-02-04 01:27:17 -0500

    few fixes

--- 3_PersonServiceImpl.java
+++ 3_PersonServiceImpl.java
@@ -2,7 +2,7 @@ package service;
 import java.util.ArrayList;
 import java.util.List;
 
-import clear.djn.DirectOptions;				//<1>
+import clear.djn.DirectOptions;			//<1>
 
 import dto.Person;
 import service.generated.*;
@@ -14,11 +14,13 @@ public class PersonServiceImpl extends _PersonServiceImpl {
 		for (int i=0; i<1000; i++){
 			result.add(new Person(i, "Joe", "Doe", "555-55-55", "1111-11-1111"));
 		}
-		int start = ((Double)DirectOptions.getOption("start")).intValue();	//<2>
+		//<2>
+		int start = ((Double)DirectOptions.getOption("start")).intValue();
 		int limit = ((Double)DirectOptions.getOption("limit")).intValue();
-		limit = Math.min(start+limit, result.size() );						//<3>
-		DirectOptions.setOption("total", result.size());					//<4>
-		result = result.subList(start, limit);								//<5>
+		
+		limit = Math.min(start+limit, result.size() );		//<3>
+		DirectOptions.setOption("total", result.size());	//<4>
+		result = result.subList(start, limit);			//<5>
 
 		return result;
 	}
